cb82c8db0f3c828b7350e578bbc981b3f86f231e
nizomovasadbek <nizomovasadbekjava@gmail.com>
2023-06-18 03:16:16 +0500

    Added cmake

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/anifetch.c.o"
+  "CMakeFiles/app.dir/src/anifetch.c.o.d"
+  "CMakeFiles/app.dir/src/lib.c.o"
+  "CMakeFiles/app.dir/src/lib.c.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
